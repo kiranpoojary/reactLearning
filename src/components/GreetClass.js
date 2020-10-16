6146265eb061b05e89fc6b707f6c30c7514f9d47
kiranpoojary <kiranpoojary483@gmail.com>
2020-10-16 17:17:09 +0530

    first Commit 16-10-2020

---
+++ src/components/GreetClass.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GreetClass extends Component{
+    render(){
+        return <div><h1>Good Morning KIRAN This is Class Component</h1></div>
+    }
+}
+
+export default GreetClass
---
+++ src/components/GreetFun.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//using functional component
+// function GreetFun(){
+//     return <h1>Hello KIRAN</h1>
+// }
+//export default Greet
+
+
+
+//same greet using arrow function
+//const GreetFun=()=> <h1>Hello KIRAN</h1>
+//export default Greet
+
+
+//same greet using arrow function with named export
+export const GreetFun=()=> <div><h1>Hello KIRAN This is Functional Componenet</h1></div>
+
+
