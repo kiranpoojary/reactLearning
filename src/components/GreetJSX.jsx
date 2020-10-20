5d6f0a0d6fa793f2d06cf70a638ba9b3f40834f3
kiranpoojary <kiranpoojary483@gmail.com>
2020-10-20 23:58:13 +0530

    JSX

---
+++ src/components/GreetJSX.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const GreetJSX=()=>{
+    return React.createElement('div',{id:'divID'},React.createElement('h1',{id:'greeting',className:'greetStyle'},'Hi This is Componet from JSX'))
+}
+
+
+export default GreetJSX
