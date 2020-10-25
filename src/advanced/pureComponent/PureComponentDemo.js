9d7e43a08e0711027b8e2647da73c9c01f840ec3
kiranpoojary <kiranpoojary483@gmail.com>
2020-10-25 11:48:38 +0530

    pure component

---
+++ src/advanced/pureComponent/PureComponentDemo.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class PureComponentDemo extends PureComponent {
+    render() {
+        return (
+            <React.Fragment>
+                Iam From Pure Component
+            </React.Fragment>
+        )
+    }
+}
+
+export default PureComponentDemo
