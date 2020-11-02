d03a3a0c6d8b5cd99bb55f81bf8663da8ea68c41
kiranpoojary <kiranpoojary483@gmail.com>
2020-11-02 18:15:52 +0530

    Hook Data Fetch Reducer

---
+++ src/hooks/fetchDatauseReducer/FetchDatauseReduceer.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useReducer } from 'react'
+import axios from 'axios'
+
+
+const initialState = {
+    loading: true,
+    error: '',
+    p0ost: {}
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            return {
+                loading: false,
+                post: action.payload,
+                error: ''
+            }
+        case 'FETCH_ERROR':
+            return {
+                loading: false,
+                post: {},
+                error: 'something went wrong'
+            }
+        default:
+            return state
+
+    }
+}
+
+function FetchDatauseReduceer() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+            .then(response => {
+                dispatch({ type: 'FETCH_SUCCESS', payload: response.data })
+
+            })
+            .catch(error => {
+                dispatch({ type: 'FETCH_ERROR' })
+
+            })
+    }, [])
+
+    return (
+        <div>
+            {state.loading ? 'Loading.....' : state.post.title}
+            {state.error ? state.error : null}
+        </div>
+    )
+}
+
+export default FetchDatauseReduceer
